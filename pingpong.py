1a37a05dfaeb38a532d653c65ff150d49a65458d
timotheyfedorov953 <timotheyfedorov953@gmail.com>
2024-10-20 14:09:56 +0300

    Add files via upload
    some changes

--- pingpong.py
+++ pingpong.py
@@ -6,19 +6,40 @@ height = 880
 window = display.set_mode((width, height))
 timer = time.Clock()
 size = 80
-class Box():
-    def __init__(self,x, y):
-        self.startx = x
-        self.starty = y
 
 
-class Player(Box):
-    def __init__(self,x, y,speed):
-        Box.__init__(self, x, y)
+class Ball():
+    def __init__(self,x, y,speed, color):
+        self.speedX = speed
+        self.speedY = speed
+        self.speed = speed
+        self.image = Surface((5,5))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(center = (x,y))
+        self.size = size
+    
+    def move(self):
+        if self.rect.x <=10:
+            self.speedX=self.speed
+
+        if self.rect.right >=width:
+            self.speedX=-self.speed
+
+        if self.rect.y <=10:
+             self.speedY=self.speed
+             
+        if self.rect.bottom >=height:
+            self.speedY=-self.speed
+        
+        self.rect.x +=self.speedX
+        self.rect.y +=self.speedY
+
+class Player():
+    def __init__(self,x, y,speed, color):
         self.speed = speed
         self.start_speed = speed 
         self.image = Surface((10, size))
-        self.image.fill((0,0,255))
+        self.image.fill(color)
         self.rect = self.image.get_rect(center = (x,y))
         self.size = size
         self.direction = 1 
@@ -36,8 +57,14 @@ class Player(Box):
         if self.rect.y <=10 or self.rect.bottom >=height:
             self.direction*=-1
 
+    def autopilot(self):
+        self.rect.y += self.speed*self.direction
+        if self.rect.y <=10 or self.rect.bottom >=height:
+            self.direction*=-1
 
-player = Player(20, height//2, 2)
+opponent = Player(width - 20, height//2, 2,(0,255,255))
+player = Player(20, height//2, 2, (255,255,255))
+ball = Ball(width//3,height//2,2, (255,0,0))
 game = True
 while game:
     window.fill('black')
@@ -48,8 +75,11 @@ while game:
         if ev.type == QUIT:
             game = False
     player.move()
-
+    opponent.autopilot()
+    ball.move()
     window.blit(player.image,(player.rect.x, player.rect.y))   
+    window.blit(opponent.image,(opponent.rect.x, opponent.rect.y))  
+    window.blit(ball.image,(ball.rect.x, ball.rect.y))    
             
 
     timer.tick(100)
